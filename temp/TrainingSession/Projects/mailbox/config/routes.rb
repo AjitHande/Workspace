230dfdab5790421620ddb034e2a2e5aa9636dde0
AjitHande <ajithande1835@gmail.com>
2021-07-04 20:33:52 +0530

    Fixed routes

--- temp/TrainingSession/Projects/mailbox/config/routes.rb
+++ temp/TrainingSession/Projects/mailbox/config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :conversations do
+    resources :messages
+  end
+
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'conversations#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
